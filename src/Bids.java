3579a592ee2712039a8abd24df9f68d607a36d13
kudesiaji <samridh.kudesia@ashoka.edu.in>
2016-04-24 01:13:57 +0530

    Deleted the entire code. Will now start from scratch. Version 2.0

--- src/Bids.java
+++
@@ -1,8 +0,0 @@
-/**
- * This class computes who is selected for which course by comparing bids for each course
- */
-public class Bids
-{
-    String studentName;
-    int studentCode;
-}
--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,1 @@
-/**
- * This class stores details of each student-their identification id, name, email id, courses allocated
- */
-public class Student
-{
-    String id;
-    String name;
-    String emailID;
-    public Student(String sCode, String sName, String sID)
-    {
-        id=sCode;
-        name=sName;
-        emailID=sID;
-    }
 
-}
